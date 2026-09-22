95dafc0732346d2367fb2a441737446518c5887f
divya-007 <divyabhadane70@gmail.com>
2026-09-22 21:14:37 +0530

    Initial commit - GenBank Online Banking System

---
+++ GenBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genbank.apnabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
